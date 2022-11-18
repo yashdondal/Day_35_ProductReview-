7ffcdfb8e32accff7d4accd0d45254d9ba9f114b
yashdondal <yashdondal21@gmail.com>
2022-11-18 20:10:38 +0530

    UC1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ProductReviewManagement
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Product Review management problem");
+            List<ProductReview> Productreviewlist = new List<ProductReview>();
+            Productreviewlist.Add(new ProductReview() { ProductID = 1, UserID = 1, Rating = 5, Review = "good", ISLike = true });
+            Productreviewlist.Add(new ProductReview() { ProductID = 2, UserID = 6, Rating = 6, Review = "good", ISLike = true });
+            Productreviewlist.Add(new ProductReview() { ProductID = 3, UserID = 2, Rating = 4, Review = "nice", ISLike = true });
+            Productreviewlist.Add(new ProductReview() { ProductID = 4, UserID = 4, Rating = 6, Review = "bad", ISLike = true });
+            Productreviewlist.Add(new ProductReview() { ProductID = 5, UserID = 2, Rating = 1, Review = "bad", ISLike = true });
+            Productreviewlist.Add(new ProductReview() { ProductID = 6, UserID = 7, Rating = 2, Review = "nice", ISLike = true });
+            Productreviewlist.Add(new ProductReview() { ProductID = 7, UserID = 8, Rating = 9, Review = "bad", ISLike = true });
+            Productreviewlist.Add(new ProductReview() { ProductID = 8, UserID = 4, Rating = 4, Review = "good", ISLike = true });
+
+
+            foreach (var list in Productreviewlist)
+            {
+                Console.WriteLine("productid : " + list.ProductID + " userid: " + list.UserID + " rating: " + list.Rating + " review: " + list.Review + " islike: " + list.ISLike);
+            }
+
+        }
+    }
+}
